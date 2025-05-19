6bcc05670ca6ca2926305610afd5e0096a199974
Tatiana <tats.krauchanka@gmail.com>
2025-05-19 17:04:26 +0300

    feat: add cva

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,13 +1,39 @@
-import { Component, input } from '@angular/core';
+import { Component, forwardRef, input } from '@angular/core';
 import { TreeModel } from '../../utils/models/tree-model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-tree',
   standalone: true,
   imports: [TreeComponent],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TreeComponent),
+      multi: true
+    }
+  ],
   templateUrl: './tree.component.html',
   styleUrl: './tree.component.scss'
 })
-export class TreeComponent {
+export class TreeComponent implements ControlValueAccessor {
   nodes = input.required<TreeModel[]>();
+  private onChange!: (value: TreeModel[]) => void;
+  private onTouched!: () => void;
+  arrOfNodes: TreeModel[] = [];
+
+  writeValue(nodes: TreeModel[]): void {
+    if (nodes) {
+      this.arrOfNodes = nodes;
+    }
+  }
+
+  registerOnChange(fn: (value: TreeModel[]) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
 }
